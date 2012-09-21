f7ca175712bbd3594bef514bc13f301b892b91b2
Fernando J. Muñoz <munozfj@hotmail.com>
2012-09-21 08:30:47 -0300

    Inicio de la aplicacion Simple Demo

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+ruby '1.9.3'
+gem  'rails', '3.2.8'
+gem  'jquery-rails'
+
+# Ambiente de desarrollo y test
+group :development do
+  gem 'sqlite3'
+end
+
+# Ambiente productivo
+group :production do
+  gem 'pg'
+  gem 'thin'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.2.3'
+end
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleDemo::Application.config.secret_token = '9cc57e7beaa6ac017daa3ba22c8f75753185e650b3c3b0c237b600ad6c3161a155afc1fca4fd420c6559c0d1b4496f02d9fcc963d82e7b5f3920eb14a1e9d573'
